d2051072330e9ccde38a9b1f0f3b5a62519782a7
ankit <ankitpathak143192@gmail.com>
2022-01-29 20:22:18 +0530

    initial commit

---
+++ components/Homepage/Hero.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <section id="hero" className=" w-full overflow-hidden p-6 lg:px-52 ">
+      <div
+        style={{ zIndex: -1 }}
+        className="absolute top-0 left-0 right-0 bottom-0"
+      >
+        <img
+          style={{ objectFit: 'cover', height: '100vh', width: '100vw' }}
+          src="/images/hero/bg.png"
+          alt="bg"
+        />
+      </div>
+      <div className="relative h-80vh w-full">
+        <div className="hero-images relative flex justify-center w-full">
+          <div
+            className="shogun-warriors absolute top-0 w-full lg:w-8/12"
+          >
+            <img src="/images/hero/img2.png" />
+          </div>
+          <div className="hero-warrior shogun-warriors w-full lg:w-10/12">
+            <img src="/images/hero/img1.png" />
+          </div>
+        </div>
+        <div
+          className="absolute info left-0 right-0 text-center text-lg text-gray-300 bottom-20 md:bottom-0 "
+        >
+          <p>
+            If they stand behind you, protect them · If they stand beside you,
+            <br />
+            respect them · If they stand against you, defeat them.
+          </p>
+          <div className="pt-4">
+            <a href="https://google.com">
+              <button className="primary btn ">
+              <span>Public Mint</span>
+              <img className="inline pl-3" src="/images/icons/arrow-next.png" />
+            </button>
+            </a>
+            <button className="btn outline-btn ">
+              <span>Join Our Discord</span>
+              <img className="inline pl-3" src="/images/icons/discord.png" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand_red: '#B80707',
+        brand_dark_red: '#A50909',
+        brand_black: '#020202',
+        brand_dark_green: '#08170D',
+        brand_gray: '#707070',
+        brand_light_green: '#0D8239',
+      },
+      opacity: {
+        10: '0.1',
+        20: '0.2',
+        30: '0.3',
+        40: '0.4',
+        50: '0.5',
+        60: '0.6',
+        70: '0.7',
+        80: '0.8',
+        90: '0.90',
+      },
+      height: {
+        '10vh': '10vh',
+        '20vh': '20vh',
+        '30vh': '30vh',
+        '40vh': '40vh',
+        '50vh': '50vh',
+        '60vh': '60vh',
+        '70vh': '70vh',
+        '80vh': '80vh',
+        '90vh': '90vh',
+      },
+    },
+  },
+  plugins: [],
+}
