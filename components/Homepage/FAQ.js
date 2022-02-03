708b377cf69f96c2befe9eecae523e1fc3019707
ankit <ankitpathak143192@gmail.com>
2022-02-03 18:25:11 +0530

    font fix

--- components/Homepage/FAQ.js
+++ components/Homepage/FAQ.js
@@ -14,7 +14,7 @@ const FAQ = () => {
     <section
       style={{ background: '#0b090a' }}
       id="faq"
-      className="relative p-6 py-16  text-white lg:px-24 xl:px-52"
+      className="relative min-h-screen p-6  py-16 text-white lg:px-24 xl:px-52 center"
     >
       <div
         style={{
@@ -32,169 +32,173 @@ const FAQ = () => {
           alt="bg"
         />
       </div>
-      <div className="relative text-center">
-        <div
-          style={{ zIndex: 2, marginTop: '-3%' }}
-          className="center absolute top-0 left-0 right-0"
-        >
-          <img src="/images/story/img3.png" />
-        </div>
-        <div style={{ zIndex: 2 }} className="relative">
-          <h3 className="brand-title-small  mb-2 uppercase text-white md:text-2xl md:text-4xl">
-            Frequently asked questions
-          </h3>
-          <h3 className="brand-title-big  text-2xl uppercase text-brand_red md:text-3xl md:text-5xl">
-            Looking for answers?
-          </h3>
-        </div>
-      </div>
-      <div className="accordion-container flex w-full flex-wrap pt-6">
-        <div className="w-full md:w-6/12">
-          <div className="lg:pr-3">
-            <Accordion
-              expanded={expanded === 'panel1'}
-              onChange={handleChange('panel1')}
-            >
-              <AccordionSummary
-                expandIcon={
-                  <div className="arrows center">
-                    <DownArrow />
-                  </div>
-                }
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <Typography>how can i get a shogun warrior?</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion
-              expanded={expanded === 'panel2'}
-              onChange={handleChange('panel2')}
-            >
-              <AccordionSummary
-                expandIcon={
-                  <div className="arrows center">
-                    <DownArrow />
-                  </div>
-                }
-                aria-controls="panel2a-content"
-                id="panel2a-header"
-              >
-                <Typography>how to buy a shogun warrior?</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion
-              expanded={expanded === 'panel3'}
-              onChange={handleChange('panel3')}
-            >
-              <AccordionSummary
-                expandIcon={
-                  <div className="arrows center">
-                    <DownArrow />
-                  </div>
-                }
-                aria-controls="panel2a-content"
-                id="panel2a-header"
-              >
-                <Typography>will these be the only colonists made?</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
+      <div className='w-full'>
+        <div className="relative text-center">
+          <div
+            style={{ zIndex: 2, marginTop: '-3%' }}
+            className="center absolute top-0 left-0 right-0"
+          >
+            <img src="/images/story/img3.png" />
+          </div>
+          <div style={{ zIndex: 2 }} className="relative">
+            <h3 className="brand-title-small  mb-2 uppercase text-white md:text-2xl md:text-4xl">
+              Frequently asked questions
+            </h3>
+            <h3 className="brand-title-big  text-2xl uppercase text-brand_red md:text-3xl md:text-5xl">
+              Looking for answers?
+            </h3>
           </div>
         </div>
-        <div className="w-full md:w-6/12">
-          <div className="lg:pl-3">
-            <Accordion
-              className="accordion-card"
-              expanded={expanded === 'panel4'}
-              onChange={handleChange('panel4')}
-            >
-              <AccordionSummary
-                className="accordion-card-title"
-                expandIcon={
-                  <div className="arrows center">
-                    <DownArrow />
-                  </div>
-                }
-                aria-controls="panel1a-content"
-                id="panel1a-header"
+        <div className="accordion-container flex w-full flex-wrap pt-6">
+          <div className="w-full md:w-6/12">
+            <div className="lg:pr-3">
+              <Accordion
+                expanded={expanded === 'panel1'}
+                onChange={handleChange('panel1')}
               >
-                <Typography>how to creae a shogun warriors?</Typography>
-              </AccordionSummary>
-              <AccordionDetails className="accordion-card-details">
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion
-              expanded={expanded === 'panel5'}
-              onChange={handleChange('panel5')}
-            >
-              <AccordionSummary
-                expandIcon={
-                  <div className="arrows center">
-                    <DownArrow />
-                  </div>
-                }
-                aria-controls="panel2a-content"
-                id="panel2a-header"
+                <AccordionSummary
+                  expandIcon={
+                    <div className="arrows center">
+                      <DownArrow />
+                    </div>
+                  }
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>how can i get a shogun warrior?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse malesuada lacus ex, sit amet blandit leo
+                    lobortis eget.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion
+                expanded={expanded === 'panel2'}
+                onChange={handleChange('panel2')}
               >
-                <Typography>how to sell a shogun warrior?</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion
-              expanded={expanded === 'panel6'}
-              onChange={handleChange('panel6')}
-            >
-              <AccordionSummary
-                expandIcon={
-                  <div className="arrows center">
-                    <DownArrow />
-                  </div>
-                }
-                aria-controls="panel2a-content"
-                id="panel2a-header"
+                <AccordionSummary
+                  expandIcon={
+                    <div className="arrows center">
+                      <DownArrow />
+                    </div>
+                  }
+                  aria-controls="panel2a-content"
+                  id="panel2a-header"
+                >
+                  <Typography>how to buy a shogun warrior?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse malesuada lacus ex, sit amet blandit leo
+                    lobortis eget.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion
+                expanded={expanded === 'panel3'}
+                onChange={handleChange('panel3')}
               >
-                <Typography>do you have a rarity system?</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
+                <AccordionSummary
+                  expandIcon={
+                    <div className="arrows center">
+                      <DownArrow />
+                    </div>
+                  }
+                  aria-controls="panel2a-content"
+                  id="panel2a-header"
+                >
+                  <Typography>
+                    will these be the only colonists made?
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse malesuada lacus ex, sit amet blandit leo
+                    lobortis eget.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </div>
+          </div>
+          <div className="w-full md:w-6/12">
+            <div className="lg:pl-3">
+              <Accordion
+                className="accordion-card"
+                expanded={expanded === 'panel4'}
+                onChange={handleChange('panel4')}
+              >
+                <AccordionSummary
+                  className="accordion-card-title"
+                  expandIcon={
+                    <div className="arrows center">
+                      <DownArrow />
+                    </div>
+                  }
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>how to creae a shogun warriors?</Typography>
+                </AccordionSummary>
+                <AccordionDetails className="accordion-card-details">
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse malesuada lacus ex, sit amet blandit leo
+                    lobortis eget.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion
+                expanded={expanded === 'panel5'}
+                onChange={handleChange('panel5')}
+              >
+                <AccordionSummary
+                  expandIcon={
+                    <div className="arrows center">
+                      <DownArrow />
+                    </div>
+                  }
+                  aria-controls="panel2a-content"
+                  id="panel2a-header"
+                >
+                  <Typography>how to sell a shogun warrior?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse malesuada lacus ex, sit amet blandit leo
+                    lobortis eget.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion
+                expanded={expanded === 'panel6'}
+                onChange={handleChange('panel6')}
+              >
+                <AccordionSummary
+                  expandIcon={
+                    <div className="arrows center">
+                      <DownArrow />
+                    </div>
+                  }
+                  aria-controls="panel2a-content"
+                  id="panel2a-header"
+                >
+                  <Typography>do you have a rarity system?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse malesuada lacus ex, sit amet blandit leo
+                    lobortis eget.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </div>
           </div>
         </div>
       </div>
